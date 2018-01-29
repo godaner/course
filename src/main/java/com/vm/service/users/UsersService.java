fc175e4b6e320fddd6cbfce8af747601b1db3678
zhangke <zhangke@wezyun.com>
2018-01-29 17:20:52 +0800

    init

---
+++ src/main/java/com/vm/service/users/UsersService.java
@@ -0,0 +1,12 @@
+package com.vm.service.users;
+
+
+
+public interface UsersService {
+    /**
+     * 添加用户
+     * @param name
+     * @param password
+     */
+    void add(String name, String password);
+}
